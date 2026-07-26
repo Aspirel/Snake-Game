ee487f3c972c31ee4100a36a0ef2fa4051fb557d
Daniel Alves <56369053+Aspirel@users.noreply.github.com>
2026-07-26 21:18:01 +0100

    Refactor Food component to use destructured props

--- src/Food.js
+++ src/Food.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
-export default (props) => {
-
+function Food({ dot }) {
   const style = {
-    left: `${props.dot[0]}%`,
-    top: `${props.dot[1]}%`
-  }
+    left: `${dot[0]}%`,
+    top: `${dot[1]}%`
+  };
 
-  return (
-    <div className="snake-food" style={style}></div>
-  )
+  return <div className="snake-food" style={style} />;
 }
+
+export default Food;
